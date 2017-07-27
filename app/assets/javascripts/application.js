c3646d6d77c60f6e66cb89ac3c8ac7f971bd764d
Katy Pola <katyp03@gmail.com>
2017-07-27 15:47:37 -0400

    Added parallax scroll functionality to home page, needs editing but framework is there

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,11 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+        $('#nav').localScroll(800);
+        $('#intro').parallax("50%", 0.1);
+        $('#second').parallax("50%", 0.1);
+        $('.bg').parallax("50%", 0.4);
+        $('#third').parallax("50%", 0.3);
+})

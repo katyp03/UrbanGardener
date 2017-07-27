c08254ef6916ad660d2335cf4f12b10e2b597578
David Zheng <davidzheng@knights.ucf.edu>
2017-07-27 11:18:13 -0400

    fixed routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_filter :configure_permitted_parameters, if: :devise_controller?
-
-  #   protected
-
-  #   def configure_permitted_parameters
-  #     devise_parameter_sanitizer.for(:sign_up) << :propic  
-  #     devise_parameter_sanitizer.for(:account_update) << :propic
-  #   end
+  def after_sign_in_path_for(resource_or_scope)
+    profile_path(current_user.username)
+  end
+  def after_sign_up_path_for(resource_or_scope)
+    profile_path(current_user.username)
+  end
 end

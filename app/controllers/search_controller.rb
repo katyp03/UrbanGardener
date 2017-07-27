c5c14409ee2846b7fad9efcadc9f524bd3003532
David Zheng <davidzheng@knights.ucf.edu>
2017-07-27 18:23:40 -0400

    autosearch fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
   def index
+    @userplant = current_user.plants
+    @userplants = []
+    @userplant.each do |plant|
+      @userplants.push(plant.name)
+    end
     # Define the search parameters
-    @search_term = params[:looking_for] || "Herbs"
+    @search_term = params[:looking_for] || @userplants
     # Change the search terms to a comma separated string if it is an array
     @search_term = @search_term.join(', ') if @search_term.is_a? Array
   	@search = Search.for(@search_term)

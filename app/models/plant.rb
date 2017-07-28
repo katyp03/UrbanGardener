983c8666de93ac71004b90a97ff9cf3238e2e98d
Katy Pola <katyp03@gmail.com>
2017-07-28 18:30:20 -0400

    set up and formatting for plant index page and partial div's

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,8 @@
 class Plant < ApplicationRecord
+	enum size: [:Small, :Medium, :Large]
 	has_many :user_plants
 	has_many :users, through: :user_plants
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/default_pic.svg"
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  	validates :size, presence: true, numericality: { in: 0..2 }
 end

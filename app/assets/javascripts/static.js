bf2eae8a1e81f31e8ef22f24852ab0232cf69ee9
Katy Pola <katyp03@gmail.com>
2017-07-28 17:49:03 -0400

    adding and formatting content (if available) to partial pages in profile

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,24 +1,26 @@
+// For profile page:
 document.addEventListener("turbolinks:load", function(){
 	const elMyPlantsLink = document.querySelector('.myPlantsLink');
 	const elMyPostsLink = document.querySelector('.myPostsLink');
 	const elMyRecipesLink = document.querySelector('.myRecipesLink');
 	if(elMyPlantsLink) {
+		// render user plants page and hide user posts and recipes when nav link is clicked:
 		elMyPlantsLink.addEventListener("click", function(){
 			document.querySelectorAll(".hidePlants").forEach(element => element.style.display="block");
 			document.querySelectorAll(".hidePosts").forEach(element => element.style.display="none");
 			document.querySelectorAll(".hideRecipes").forEach(element => element.style.display="none");
 		});
 	};
-	// const elMyPostsLink = document.querySelector('.myPostsLink');
 	if(elMyPostsLink) {
+		// render user posts page and hide user plants and recipes when nav link is clicked:
 		elMyPostsLink.addEventListener("click", function(){
 			document.querySelectorAll(".hidePosts").forEach(element => element.style.display="block");
 			document.querySelectorAll(".hidePlants").forEach(element => element.style.display="none");
 			document.querySelectorAll(".hideRecipes").forEach(element => element.style.display="none");
 		});
 	};
-	// const elMyRecipesLink = document.querySelector('.myRecipesLink');
 	if(elMyRecipesLink) {
+		// render user recipes page and hide user posts and plants when nav link is clicked:
 		elMyRecipesLink.addEventListener("click", function(){
 			document.querySelectorAll(".hideRecipes").forEach(element => element.style.display="block");
 			document.querySelectorAll(".hidePlants").forEach(element => element.style.display="none");

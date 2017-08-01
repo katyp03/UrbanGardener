544bd47d02e4d2e4682e8e835c2d2f60f9dcaa41
David Zheng <davidzheng@knights.ucf.edu>
2017-08-01 11:22:59 -0400

    parallax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,7 +46,8 @@ document.addEventListener("turbolinks:load", function(){
   $('#nav').localScroll(800);
   $('#intro').parallax("50%", 0.1);
   $('#second').parallax("50%", 0.1);
-  $('.bg').parallax("30%", 0.4);
+  $('.bg').parallax("30%", 3);
+  $('.sc').parallax("8%", 2.4);
   $('#third').parallax("50%", 0.3);
 });
 
